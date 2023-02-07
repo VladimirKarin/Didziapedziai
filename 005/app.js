89221bcbb81d517f040207861c8fdd5aa7d53942
Vladimir Karin <Vladimir.Karin@gmail.com>
2023-02-07 12:57:58 +0200

    localStorage

---
+++ 005/app.js
@@ -0,0 +1,8 @@
+console.log('005 Much wow');
+// Local storage
+
+let color = 'skyblue';
+localStorage.setItem('myFavoriteColor', color);
+// localStorage.removeItem('myFavoriteColor'); Galima istrinti
+
+console.log(localStorage.getItem('myFavoriteColor'));
